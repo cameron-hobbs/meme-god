c8e6969e18c992221665d401237fd0fcf47bf925
cameronthealgo <camerontech@protonmail.com>
2025-04-20 02:38:17 +0800

    Fix reddit user name

--- src/reddit/admin/user.py
+++ src/reddit/admin/user.py
@@ -8,5 +8,9 @@ from src.reddit.models import RedditUser
 
 @admin.register(RedditUser)
 class RedditUserAdmin(ReadOnlyModelAdmin):
-    fields = ("full_name",)
-    list_display = ("full_name",)
+    fields = (
+        "name",
+        "full_name",
+    )
+    list_display = ("id", "name", "created_at")
+    # todo: inline for posts
